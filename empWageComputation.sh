861e21a7ef95e33a33ab08b50ca43723a6c1008c
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-24 19:31:44 +0530

    MODIFIED: Calculated daily wages of employess

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
-printf "Welcome to Employee Wage Computation\n"
+printf "Calculate daily wages\n"
+#CONSTANTS
+WAGEPERHOUR=20
+FULLDAYHOURS=8
+#VARIABLES
 isPresent=1
+wage=0
 random=$((RANDOM%2))
 if(($random==$isPresent))
 then
 	printf "Employee is present\n"
+	wage=$((WAGEPERHOUR*FULLDAYHOURS))
+	printf "$wage"
 else
 	printf "Employee is absent\n"
+	printf "$wage"
 fi
