c577069ebe678d09f3df00278cdce8a1d2da88ff
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-25 10:44:08 +0530

    MODIFIED: Added monthly calculation of wages for employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,33 @@
 #!/bin/bash -x
-printf "Added case statements to part time feature\n"
+printf "Calculating monthly wages for employee\n"
 #CONSTANTS
 WAGEPERHOUR=20
+WORKINGDAYS=20
 #VARIABLES
 hoursPerDay=0
 isPartTime=1
 isPresent=2
 wage=0
-random=$((RANDOM%3))
-case $random in 
-	$isPresent)
-		printf "Employee is full time present\n"
-		hoursPerDay=8
-		;;
-	$isPartTime)
-		printf "Employee is part time present\n"
-		hoursPerDay=4
-		;;
-	*)
-		printf "Employee is absent\n"
-		hoursPerDay=0
-		;;
-esac
-wage=$((WAGEPERHOUR*hoursPerDay))
+monthlyWage=0
+#CALCULATING MONTHLY WAGES IN THE FORLOOP
+for((days=1; days<=$WORKINGDAYS; days++))
+do
+	random=$((RANDOM%3))
+	case $random in 
+		$isPresent)
+			printf "Employee is full time present\n"
+			hoursPerDay=8
+			;;
+		$isPartTime)
+			printf "Employee is part time present\n"
+			hoursPerDay=4
+			;;
+		*)
+			printf "Employee is absent\n"
+			hoursPerDay=0
+			;;
+	esac
+	wage=$((WAGEPERHOUR*hoursPerDay))
+	monthlyWage=$((monthlyWage+wage))
+done
+printf "Month wage: $monthlyWage\n"
